2bb0f55c46ab6bda496c8fca2fe2448bf977cc9a
AndreanVeselinov <47809581+AndreanVeselinov@users.noreply.github.com>
2026-04-01 14:09:39 +0300

    Sync from Azure DevOps at commit 12cb31f44cf6a832bbd48f6faf0de390a3a3b07f (#74)

--- src/js/okta.js
+++ src/js/okta.js
@@ -8,20 +8,14 @@ const oktaAuth = new OktaAuth({
 });
 
 async function loginOktaUser() {
-console.log('login okta user called');
-console.log(oktaAuth.tokenManager.get('accessToken'));
-
     
 if (oktaAuth.isLoginRedirect()) {
-    console.log('coming from redirect');
     return;
   }
 
   // 2) Already have tokens in storage?
-  console.log('getting tokens from token manager');
   const tokens = await oktaAuth.tokenManager.getTokens();
   if (tokens?.accessToken && tokens?.idToken) {
-    console.log('already have tokens');
     return tokens;
   }
 
@@ -36,28 +30,20 @@ if (employeeLoginBtn) {
 }
 
 (function () {
-    console.log('check if okta code is set');
     // Parse query parameters
     const params = new URLSearchParams(window.location.search);
     const variable = params.get("code");
 
     if (variable) {
 
-        console.log('variable is set');
-        // Save to cookie (1 day)
-        document.cookie = `EAVDVSCookie=${variable}; path=/; max-age=86400`;
-
+        document.cookie = `EAVDVSCookie=${variable}; path=/;`;
         // Remove query from address bar
         const cleanUrl = window.location.origin + window.location.pathname;
         window.history.replaceState({}, "", cleanUrl);
         window.dispatchEvent(new CustomEvent('validation-token-set', { detail: { token: 'null' } }));
-        console.log('event dispatched ')
         window.addEventListener("DOMContentLoaded", () => {
-        console.log('dom content loaded in okta js')
         if (window.setTokenForAddressValidation) {
-            console.log('adding token from okta js');
             const result = window.setTokenForAddressValidation("okta");
-            console.log("Experian result:", result);
         } else {
             console.error("Experian script not loaded!");
         }
