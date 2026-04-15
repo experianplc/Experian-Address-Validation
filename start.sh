0fc566914127501bdd1ea897b66696d330e4b906
Experian Data Quality <edq@experian.com>
2026-04-15 14:06:27 +0000

    Sync from Azure DevOps at commit 50a436d08a96cbcf369cf1fe84f9a87ba07201d3

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-mkdir -p /app/logs
-
 # 1) Replace placeholders in static assets
 find /app \
   -path '/app/node_modules' -prune -o \
@@ -14,9 +12,6 @@ find /app -name 'okta.js' -type f -print0 | xargs -0 -r sed -i \
   -e "s|%OKTA_ISSUER%|${OKTA_ISSUER:-}|g" \
   -e "s|%OKTA_REDIRECT_URI%|${OKTA_REDIRECT_URI:-}|g"
 
-# remove logs
-rm -rf /app/logs/*.log
-touch /app/logs/serve.log
 # 4) Start 'serve' in foreground (container main process)
 echo "[entrypoint] starting static server on ${PORT} ..."
-exec serve -s /app -l "tcp://0.0.0.0:${PORT}" --no-clipboard --no-port-switching 2>&1 | tee -a /app/logs/serve.log
+exec serve -s /app -l "tcp://0.0.0.0:${PORT}" --no-clipboard --no-port-switching 2>&1 | tee -a /app/serve.log
